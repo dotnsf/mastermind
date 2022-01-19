3cafd63d18d88170453f01d7c9bb68d26a881499
K.Kimura <dotnsf@gmail.com>
2022-01-19 09:29:38 +0900

    #10 applied

--- public/mmrestapi.js
+++ public/mmrestapi.js
@@ -1,4 +1,23 @@
 
+var base_url = 'https://mastermind-restapi.herokuapp.com';
+
+async function mmPing(){
+  return new Promise( function( resolve, reject ){
+    $.ajax({
+      type: "GET",
+      url: base_url + "/api/ping",
+      success: function( result ){
+        console.log( 'mmPing', result );
+        resolve( result );
+      },
+      error: function( e0, e1, e2 ){
+        console.log( e0, e1, e2 );
+        resolve( null );
+      }
+    });
+  });
+}
+
 async function mmInit( length = 4, highlow = 0 ){
   return new Promise( function( resolve, reject ){
     $.ajax({
@@ -66,3 +85,21 @@ async function mmStatus( id ){
     });
   });
 }
+
+function timestamp2datetime( ts ){
+  if( ts ){
+    var dt = new Date( ts );
+    var yyyy = dt.getFullYear();
+    var mm = dt.getMonth() + 1;
+    var dd = dt.getDate();
+    var hh = dt.getHours();
+    var nn = dt.getMinutes();
+    var ss = dt.getSeconds();
+    var datetime = yyyy + '-' + ( mm < 10 ? '0' : '' ) + mm + '-' + ( dd < 10 ? '0' : '' ) + dd
+      + ' ' + ( hh < 10 ? '0' : '' ) + hh + ':' + ( nn < 10 ? '0' : '' ) + nn + ':' + ( ss < 10 ? '0' : '' ) + ss;
+    return datetime;
+  }else{
+    return "";
+  }
+}
+
